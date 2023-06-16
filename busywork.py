936e2562fd3f1b38f98c709644aa64ebc373f197
otherfutures <109718981+otherfutures@users.noreply.github.com>
2023-06-17 03:09:57 +0900

    Self-edit: Add current date

--- busywork.py
+++ busywork.py
@@ -47,6 +47,7 @@ def make_edit(script_path):
         file.writelines(lines)
 
     # Use Git commands to stage, commit, and push the changes
+    os.system("git pull")
     os.system("git add .")
     os.system(f'git commit -m "{EDIT_MESSAGE}"')
     os.system("git push")
@@ -66,6 +67,7 @@ def revert_edit(script_path):
         file.writelines(lines)
 
     # Use Git commands to stage, commit, and push the revision
+    os.system("git pull")
     os.system("git add .")
     os.system(f'git commit -m "{REVERT_MESSAGE}"')
     os.system("git push")
@@ -81,3 +83,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# Pointless Edit: 2023-06-17
